efc9a95a432fdb5da36050b0cae9a29857904a3e
Javier García <javigcpgr@gmail.com>
2023-08-25 15:21:04 +0200

    feat(HomePage)

--- client/src/routes/RoutesPath.routes.tsx
+++ client/src/routes/RoutesPath.routes.tsx
@@ -1,7 +1,17 @@
 
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage } from '../pages/HomePage'
+import { LoginPage } from '../pages/LoginPage'
 export const RoutesPath = () => {
     return (
-        <div>RoutesPath.routes</div>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'>
+                        <Route index element={<LoginPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
